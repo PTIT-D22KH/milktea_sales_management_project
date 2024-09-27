2aafb03ed681136273fc3b6bbba61db0e8be68d8
Tinab <trandinhthien0104@gmail.com>
2024-09-27 10:29:12 +0700

    Add Employee.java and FoodCategory.java

---
+++ src/main/java/models/Employee.java
@@ -0,0 +1,87 @@
+package models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author TP
+ */
+public class Employee extends Model {
+    private int id, salary;
+    private String username, password, name, phoneNumber;
+    private String permission;
+    private Date startDate;
+    
+    public Employee(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    
+    @Override
+    public String toString(){
+        return "";
+    }
+}
---
+++ src/main/java/models/FoodCategory.java
@@ -0,0 +1,44 @@
+package models;
+
+/**
+ *
+ * @author TP
+ */
+public class FoodCategory extends Model {
+    private int id;
+    private String name;
+    private String slug;
+    
+    public FoodCategory(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+    
+    @Override
+    public String toString(){
+        return "";
+    }
+}
