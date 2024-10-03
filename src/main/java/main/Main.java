493505266e704f36909ac0eaf8fc65f4c2413e20
BuiVannn <buivanphuctho@gmail.com>
2024-10-03 23:07:07 +0700

    cm

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -15,7 +15,11 @@ import models.FoodCategory;
 import utils.EmployeePermission;
 import dao.EmployeeDao;
 import dao.FoodCategoryDao;
+import dao.FoodItemDao;
+import dao.OrderItemDao;
 import dao.TableDao;
+import models.FoodItem;
+import models.OrderItem;
 import models.Table;
 import utils.TableStatus;
 
@@ -29,6 +33,20 @@ public class Main {
         System.out.println("Ket noi csdl thanh cong!");
 //        CustomerDao customerDao = new CustomerDao();
 //        customerDao.deleteById(1);
-        
+        try {
+            OrderItemDao dao = new OrderItemDao();
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrderId(8); // Thay đổi giá trị này theo nhu cầu của bạn
+            orderItem.setFoodItemId(2);
+            orderItem.setToppingId(3);
+            orderItem.setQuantity(4);
+            orderItem.setFoodPrice(150);
+            orderItem.setToppingPrice(30);
+            orderItem.setNote("Less spicy");
+            dao.update(orderItem);
+            System.out.println("Order item updated successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
