470f53e37f102ddc7016c4bd4266d2101ed40105
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2024-10-14 16:59:55 +0700

    Finish SidebarController, add OrderManagerController and abstract controller manager classes * Add OrderManagerController * Add ShipmentManagerController * Add AddOrderPopupController * Add EditOrderPopupController * Add OrderPopupController * Add SelectCustomerPopupController * Add SelectEmployeePopupController * Add ShipmentPopupControler * Add SelectEntityPopupController * Add EntityRenderList * Add BaseSelectEntityPopupView * Add SelectEntityPopupView * Add EditOrderPopupView * Add SelectCustomerPopupView * Add SelectEmployeePopupView * Add SelectEntityPopupView

--- src/main/java/controllers/popup/SelectEmployeePopupController.java
+++ src/main/java/controllers/popup/SelectEmployeePopupController.java
@@ -5,56 +5,61 @@
 package controllers.popup;
 
 import dao.EmployeeDao;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import javax.swing.JFrame;
 import models.Employee;
 import views.popup.SelectEmployeePopupView;
+import views.popup.SelectEntityPopupView;
 
 /**
  *
  * @author buiva
  */
-public class SelectEmployeePopupController {
-    EmployeeDao employeeDao = new EmployeeDao();
-    JFrame previousView;
-    
-    public interface Callback {
-        public abstract void run(Employee employee);
+public class SelectEmployeePopupController extends SelectEntityPopupController<SelectEmployeePopupView, EmployeeDao, Employee>{
+//    private EmployeeDao employeeDao;
+//    private JFrame previousView;
+    public SelectEmployeePopupController() {
+        this.entityDao = new EmployeeDao();
     }
+//
     
-    public void select(SelectEmployeePopupView view, Callback callback) {
-        if(previousView != null && previousView.isDisplayable()) {
-            previousView.requestFocus();
-            return;
-        }
-        previousView = view;
-        view.setVisible(true);
-        try {         
-            view.renderEmployee(employeeDao.getAll());
-        }
-        catch (SQLException e) {
-            view.showError(e);
-        }
-        view.getBtnOK().addActionListener(evt -> {
-            Employee c = view.getListEmployee().getSelectedValue();
-            if(c == null) {
-                view.showError("Ban chua chon nhan vien nao");
-                return;
+    public SelectEmployeePopupController(EmployeeDao employeeDao) {
+        this.entityDao = employeeDao;
+    }
+//    
+//    public interface Callback {
+//        public abstract void run(Employee employee);
+//    }
+    @Override
+    public void select(SelectEmployeePopupView view, Callback<Employee> callback) {
+        super.select(view, callback);
+        view.getBtnOK().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Employee c = view.getEntityList().getSelectedValue();
+                if(c == null) {
+                    view.showError("Bạn chưa chọn nhân viên nào!");
+                    return;
+                }
+                callback.run(c);
+                view.dispose();
             }
-            callback.run(c);
-            view.dispose();
+            
         });
-        view.getBtnSearch().addActionListener(evt -> {
-            String txtSearch = view.getTxtEmployeeName().getText();
-            try {
-                view.renderEmployee(employeeDao.searchByKey("name", txtSearch));          
+        view.getBtnSearch().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String txtSearch = view.getEntityNameTxtField().getText();
+                try {
+                    view.renderEntity(entityDao.searchByKey("name", txtSearch));          
+                }
+                catch (Exception exception) {
+                    view.showError(exception);
+                }
             }
-            catch (Exception e) {
-                view.showError(e);
-            }
-        });
-        view.getBtnCancel().addActionListener(evt -> {
-            view.dispose();
+            
         });
     }
 }
