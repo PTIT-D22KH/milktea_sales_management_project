169d504851e7be7f463f7a9bae90c4441dc3332c
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2024-10-22 15:14:55 +0700

    edit database insert data queries, add StatisticalDao and Statistical, Workdayinfo view and workdayinfo controller

---
+++ src/main/java/models/statistical/WorkingMinus.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models.statistical;
+
+import java.sql.Date;
+import models.Employee;
+
+/**
+ *
+ * @author P51
+ */
+public class WorkingMinus {
+    private Employee employee;
+    private Date date;
+    private int minus;
+
+    public WorkingMinus(Employee employee, Date date, int minus) {
+        this.employee = employee;
+        this.date = date;
+        this.minus = minus;
+    }
+    
+    
+}
