d40102dc0adb05047b5ece5c6c1f20447f8fa6f8
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2024-10-22 20:23:02 +0700

    Delete SubMenuView

--- src/main/java/controllers/SidebarController.java
+++ src/main/java/controllers/SidebarController.java
@@ -51,16 +51,9 @@ public class SidebarController {
     public void addMenu(MenuItemView... menu) {
         for (int i = 0; i < menu.length; i++) {
             MenuItemView item = menu[i];
-            
             menuItems.add(item);
-            item.setActive(false);
             sidebarPanel.add(item);
             sidebarPanel.setVisible(true);
-            ArrayList<MenuItemView> subMenus = item.getSubMenu();
-            for (MenuItemView subMenu : subMenus) {
-                addMenu(subMenu);
-                subMenu.setVisible(false);
-            }
         }
     }
     
@@ -78,43 +71,8 @@ public class SidebarController {
         }
     }
 
-    public void renderMenu() {
-        for (MenuItemView menuItem : menuItems) {
-            MenuItemView parrent = menuItem.getParentMenu();
-            if (parrent == null) { // Nếu là menu cha trên cùng
-                menuItem.setVisible(true);
-            } else if (parrent.isActive()) {//Nếu cha được chọn thì show con ra
-                menuItem.setVisible(true);
-            } else if (menuItem.isActive()) {//Nếu đang chọn thì show ra
-                menuItem.setVisible(true);
-            } else {
-                menuItem.setVisible(false);
-            }
-        }
-    }
-
-    private void closePreviosMenu(MenuItemView previousItem) {//Đóng menu cũ
-        MenuItemView parent = previousItem.getParentMenu();
-        previousItem.setActive(false);
-        while (parent != null) {
-            parent.setActive(false);
-            parent = parent.getParentMenu();
-        }
-    }
-
     public void setMenu(MenuItemView item) {//Chọn menu
-        boolean isActive = item.isActive();
-        if (activeMenuItemView != null) {
-            closePreviosMenu(activeMenuItemView);
-        }
-        MenuItemView parent = item.getParentMenu();
-        while (parent != null) {
-            parent.setActive(true);
-            parent = parent.getParentMenu();
-        }
-        item.setActive(!isActive);
         activeMenuItemView = item;
-        renderMenu();
     }
     
 }
--- src/test/java/models/CustomerTest.java
+++ src/test/java/models/CustomerTest.java
@@ -1,88 +1,88 @@
-///*
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-// * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
-// */
-//package models;
-//
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.AfterAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-///**
-// *
-// * @author P51
-// */
-//public class CustomerTest {
-//    public CustomerTest() {
-//        
-//    }
-//    
-//    @BeforeAll
-//    public static void setUpClass() {
-//        
-//    }
-//    
-//    @AfterAll
-//    public static void tearDownClass() {
-//    }
-//    
-//    @BeforeEach
-//    public void setUp() {
-//        
-//    }
-//    
-//    @AfterEach
-//    public void tearDown() {
-//    }
-//
-//    // TODO add test methods here.
-//    // The methods must be annotated with annotation @Test. For example:
-//    //
-//    // @Test
-//    // public void hello() {}
-//    @Test
-//    public void testConstructorNoParameter() {
-//        Customer customer = new Customer();
-//        assertEquals(customer.getAddress(), "");
-//        assertEquals(customer.getName(), "");
-//        assertEquals(customer.getPhoneNumber(), "");
-//    }
-//    
-//    @Test
-//    public void testGetterAndSetterCustomerId() {
-//        Customer customer = new Customer();
-//        int customerId = 1;
-//        customer.setCustomerId(customerId);
-//        assertEquals(customer.getCustomerId(), customerId);
-//    }
-//    
-//    @Test
-//    public void testGetterAndSetterAddress() {
-//        Customer customer = new Customer();
-//        String address = "Hanoi";
-//        customer.setAddress(address);
-//        assertEquals(customer.getAddress(), address);
-//    }
-//    
-//    @Test
-//    public void testGetterAndSetterName() {
-//        Customer customer = new Customer();
-//        String name = "Nguyen Van A";
-//        customer.setName(name);
-//        assertEquals(customer.getName(), name);
-//    }
-//    
-//    @Test
-//    public void testGetterAndSetterPhoneNumber() {
-//        Customer customer = new Customer();
-//        String phoneNumber = "09007508";
-//        customer.setPhoneNumber(phoneNumber);
-//        assertEquals(customer.getPhoneNumber(), phoneNumber);
-//    }
-//    
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author P51
+ */
+public class CustomerTest {
+    public CustomerTest() {
+        
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+        
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+        
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void testConstructorNoParameter() {
+        Customer customer = new Customer();
+        assertEquals(customer.getAddress(), "");
+        assertEquals(customer.getName(), "");
+        assertEquals(customer.getPhoneNumber(), "");
+    }
+    
+    @Test
+    public void testGetterAndSetterCustomerId() {
+        Customer customer = new Customer();
+        int customerId = 1;
+        customer.setCustomerId(customerId);
+        assertEquals(customer.getCustomerId(), customerId);
+    }
+    
+    @Test
+    public void testGetterAndSetterAddress() {
+        Customer customer = new Customer();
+        String address = "Hanoi";
+        customer.setAddress(address);
+        assertEquals(customer.getAddress(), address);
+    }
+    
+    @Test
+    public void testGetterAndSetterName() {
+        Customer customer = new Customer();
+        String name = "Nguyen Van A";
+        customer.setName(name);
+        assertEquals(customer.getName(), name);
+    }
+    
+    @Test
+    public void testGetterAndSetterPhoneNumber() {
+        Customer customer = new Customer();
+        String phoneNumber = "09007508";
+        customer.setPhoneNumber(phoneNumber);
+        assertEquals(customer.getPhoneNumber(), phoneNumber);
+    }
+    
 //    @Test
 //    public void testToString() {
 //        int customerId = 1;
@@ -98,4 +98,4 @@
 ////        System.out.println(res);
 //        assertEquals(customer.toString(), res);
 //    }
-//}
+}
